a224d173d70784bdaabae76cf2cfa42c6f35026f
6669venus <venus6669@yandex.com>
2017-08-27 12:37:57 +0000

    Add materizecss

--- studydjango/settings.py
+++ studydjango/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'studydjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ykkkrvwr',
+        'USER': 'ykkkrvwr',
+        'PASSWORD': 'vOrDTN80OioVUVc3xdbzq5Y5Xd10lDg2',
+        'HOST': 'babar.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
