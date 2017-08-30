79adb33276e08e9656ea81555fd8741548cac760
6669venus <venus6669@yandex.com>
2017-08-30 12:30:32 +0000

    Add customcommands

--- customcommands/management/commands/collectall.py
+++ customcommands/management/commands/collectall.py
@@ -6,35 +6,26 @@ import os
 from subprocess import call
 
 class Command(BaseCommand):
-    """
-    Make sure you are already install libsass-python <pip install libsass>
-    
-    STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_DIRS = [
-        "public",
-    ]
-
-    INSTALLED_APPS = [
-        'custom_commands',
-    ]
-    
-    """
     help = 'Call collectstatic and then sassc'
 
     def handle(self, *args, **options):
-        call_command('collectstatic', verbosity=0, interactive=False)
-        
-        
-        csspath = os.path.join(settings.STATIC_ROOT, os.path.join('web','css'))
-        sasspath = os.path.join(settings.STATIC_ROOT, 'sass')
-        #csspath = settings.STATIC_ROOT + '/css'
+        csspath =  os.path.join(os.path.join(settings.PROJECT_ROOT, 'static'), 'css')
+        sasspath =  os.path.join(os.path.join(settings.PROJECT_ROOT, 'static'), 'sass')
+
         for file in os.listdir(sasspath):
             if file.endswith(".scss"):
-                cmd1 = 'sassc ' + os.path.join(sasspath, file) + ' > ' + os.path.join(csspath, os.path.splitext(file)[0] + '.css')
+                cmd1 = 'sass ' + os.path.join(sasspath, file) + ' > ' + os.path.join(csspath, os.path.splitext(file)[0] + '.css')
                 print cmd1
                 call(cmd1, shell=True)
-                os.remove(os.path.join(sasspath, file))
-        os.rmdir(sasspath)
+                
+                cmd2 = 'sass ' + os.path.join(sasspath, file) + ' --style compressed > ' + os.path.join(csspath, os.path.splitext(file)[0] + '.min.css')
+                print cmd2
+                call(cmd2, shell=True)
+                
+                # os.remove(os.path.join(sasspath, file))
+        # os.rmdir(sasspath)
+        
+        call_command('collectstatic', verbosity=0, interactive=False)
+                
         self.stdout.write(self.style.SUCCESS('Successfully'))
         
