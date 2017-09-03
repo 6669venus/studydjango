fb28e9c060c9080b4160cfef7d5b428215ee0c45
6669venus <venus6669@yandex.com>
2017-09-03 14:14:18 +0000

    Add more

--- myip/models.py
+++ myip/models.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class MyIP(models.Model):
+    created_date = models.DateTimeField(default=timezone.now)
+    myipaddress = models.TextField()
+
+    def __str__(self):
+        return self.myipaddress + ' ' + str(self.created_date)
--- myip/views.py
+++ myip/views.py
@@ -2,6 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import MyIP
 
 def index(request):
-    return render(request, 'myip/index.html', {})
+    myips = MyIP.objects.all().order_by('-created_date')
+    yourip = "111.222.333.111/233.535.66.77"
+    return render(request, 'myip/index.html', {'myips': myips, 'yourip': yourip})
+    #return render(request, 'myip/index.html', {'yourip': yourip})
+    
