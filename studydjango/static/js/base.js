c8ad2bf2b92a66b07c77592928867759b3be57f4
6669venus <venus6669@yandex.com>
2017-09-05 13:26:27 +0000

    2331

--- studydjango/static/js/base.js
+++ studydjango/static/js/base.js
@@ -7,7 +7,6 @@ function baseInit() {
                     var cookies = document.cookie.split(';');
                     for (var i = 0; i < cookies.length; i++) {
                         var cookie = jQuery.trim(cookies[i]);
-                        // Does this cookie string begin with the name we want?
                         if (cookie.substring(0, name.length + 1) == (name + '=')) {
                             cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                             break;
@@ -17,7 +16,6 @@ function baseInit() {
                 return cookieValue;
             }
             if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-                // Only send the token to relative URLs i.e. locally.
                 xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
             }
         }
